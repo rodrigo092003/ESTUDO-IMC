e99e23fd76cd51300ef4cb452b39f4316d1a090c
Rodrigoi França <francarodrigo003@gmail.com>
2025-08-12 21:16:10 +0000

    segundo teste - continuar

--- script.js
+++ script.js
@@ -3,14 +3,15 @@ function calcularIMC(peso, altura) {
         return "Peso e altura devem ser maiores que zero.";
     }
     var imc = peso / (altura * altura);
-    res = `Seu IMC é ${imc}.`;
-    if imc < 18.5{
-        res.concat(" Você está abaixo do peso ideal.");
-    }else if (imc >= 18.5 && imc < 24.9) {
-        res.concat(" Você está no peso ideal.");
+    var res = `Seu IMC é ${imc}.`;
+    if (imc < 18.5) {
+        res += " Você está abaixo do peso ideal.";
+    } else if (imc >= 18.5 && imc < 24.9) {
+        res += " Você está no peso ideal.";
     } else if (imc >= 25 && imc < 29.9) {
-        res.concat(" Você está com sobrepeso.");
+        res += " Você está com sobrepeso.";
     } else {
-        res.concat(" Você está com obesidade.");
+        res += " Você está com obesidade.";
     }
+    return res;
 }
